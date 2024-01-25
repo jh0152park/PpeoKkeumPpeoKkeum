41558954182dd2e1146287b205250c2785cb3d43
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-01-25 18:20:09 +0900

    update code

--- ppeokkeum/src/screens/Main.tsx
+++ ppeokkeum/src/screens/Main.tsx
@@ -1,5 +1,20 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading, VStack } from "@chakra-ui/react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function Main() {
-    return <Heading>MAIN</Heading>;
+    return (
+        <VStack w="100%" h="100vh">
+            <Heading>MAIN</Heading>
+            <Box w="100%" h="100%">
+                <Map
+                    center={{ lat: 33.5563, lng: 126.79581 }}
+                    style={{ width: "100%", height: "360px" }}
+                >
+                    <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+                        <div style={{ color: "#000" }}>Hello World!</div>
+                    </MapMarker>
+                </Map>
+            </Box>
+        </VStack>
+    );
 }
